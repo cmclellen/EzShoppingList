e9ed294278e94c4ac7bbf48ea3b4f2f6e65f0784
Craig McLellen <cmclellen@gmail.com>
2025-01-02 17:31:29 +0800

    Fix

--- src/pages/Lists.tsx
+++ src/pages/Lists.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import getShoppingLists from "../services/apiShoppingLists";
+
+interface ShoppingList {
+  id: number;
+  name: string;
+}
+
 function Lists() {
-  return <div>ShoppingsLists</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [shoppingLists, setShoppingLists] = useState<ShoppingList[]>();
+
+  useEffect(() => {
+    setIsLoading(true);
+    getShoppingLists().then((data) => {
+      setShoppingLists(data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) return <span>Loading...</span>;
+
+  return (
+    <ul>
+      {shoppingLists &&
+        shoppingLists.map((list) => <li key={list.id}>{list.name}</li>)}
+    </ul>
+  );
 }
 
 export default Lists;
