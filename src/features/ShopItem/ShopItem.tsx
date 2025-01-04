041737fdc377ff8abe5ee8f7b14cae3c751cd148
Craig McLellen <cmclellen@gmail.com>
2025-01-04 13:16:28 +0800

    Wire-up shop item check (#7)

--- src/features/ShopItem/ShopItem.tsx
+++ src/features/ShopItem/ShopItem.tsx
@@ -1,25 +1,41 @@
 import { Checkbox } from "@headlessui/react";
-import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { FaCheck } from "react-icons/fa";
+import { updateShopItemCompletedStatus } from "../../services/apiShopItem";
+import toast from "react-hot-toast";
+import QueryKey from "../../utils/queryKeys";
 
 interface ShopItemProps {
+  id: number;
   name: string;
   completed: boolean;
 }
 
-function ShopItem({ name, completed }: ShopItemProps) {
-  const [isCompleted, setIsCompleted] = useState(completed);
+function ShopItem({ id, name, completed }: ShopItemProps) {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: (completed: boolean) =>
+      updateShopItemCompletedStatus(id, completed),
+    onSuccess: () => {
+      toast.success("Shop item marked as completed");
+      queryClient.invalidateQueries({ queryKey: [QueryKey.SHOPPING_LIST] });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
 
   return (
     <li className="font-semibold flex gap-2 items-center py-5">
       <Checkbox
-        checked={isCompleted}
-        onChange={setIsCompleted}
+        checked={completed}
+        onChange={(e) => mutate(e)}
         className="group size-6 rounded-md bg-primary/10 p-1 ring-1 ring-primary/15 ring-inset data-[checked]:bg-primary/10"
       >
         <FaCheck className="hidden size-4 fill-primary group-data-[checked]:block" />
       </Checkbox>
-      <span className={isCompleted ? "line-through" : ""}>{name}</span>
+      <span className={completed ? "line-through" : ""}>{name}</span>
     </li>
   );
 }
