c6ac7df44c2f2a916fae049e4a5b43cd1466c4ca
Craig McLellen <cmclellen@gmail.com>
2025-01-05 08:43:49 +0800

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import ShoppingList from "./pages/ShoppingList";
-import Login from "./pages/Login";
 import LoginForm from "./features/authentication/LoginForm";
 import SignupForm from "./features/authentication/SignupForm";
 import FullPage from "./ui/FullPage";
@@ -39,7 +38,7 @@ function App() {
             <Route
               path="signup"
               element={
-                <FullPage title="Signup">
+                <FullPage title="Sign up">
                   <SignupForm />
                 </FullPage>
               }
